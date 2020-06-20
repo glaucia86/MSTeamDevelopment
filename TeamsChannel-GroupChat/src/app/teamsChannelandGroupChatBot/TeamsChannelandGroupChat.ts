5fdb66d4fd63cfc0bfef3d25a478ddda30052b6b
Manoj Mittal <manoj.mcans@hotmail.com>
2020-06-21 00:05:59 +0530

    Add Conversation bot and get context within Microsoft Teams Channel and Group Chat
    Conversational Bot can do many thing with in MS Teams Client.
    Personal Conversation or Personal Chat : It's one to one chat between users and bot.
    Group Conversation or Group Chat : It's between bot and two or more users.
    Teams Channel: It's available to all channel team members, any team members can do conversation.

---
+++ TeamsChannel-GroupChat/src/app/teamsChannelandGroupChatBot/TeamsChannelandGroupChat.ts
@@ -0,0 +1,141 @@
+import { BotDeclaration, MessageExtensionDeclaration, PreventIframe } from "express-msteams-host";
+import * as debug from "debug";
+import { DialogSet, DialogState } from "botbuilder-dialogs";
+import { StatePropertyAccessor,MessageFactory, CardFactory, TurnContext, MemoryStorage, ConversationState, ActivityTypes, TeamsActivityHandler } from "botbuilder";
+import HelpDialog from "./dialogs/HelpDialog";
+import WelcomeCard from "./dialogs/WelcomeDialog";
+import * as Util from "util";
+const TextEncoder = Util.TextEncoder;
+// Initialize debug logging module
+const log = debug("msteams");
+
+/**
+ * Implementation for TeamsChannelandGroupChat
+ */
+@BotDeclaration(
+    "/api/messages",
+    new MemoryStorage(),
+    process.env.MICROSOFT_APP_ID,
+    process.env.MICROSOFT_APP_PASSWORD)
+
+export class TeamsChannelandGroupChat extends TeamsActivityHandler {
+   
+    
+    
+    private readonly conversationState: ConversationState;
+    private readonly dialogs: DialogSet;
+    private dialogState: StatePropertyAccessor<DialogState>;
+
+    /**
+     * The constructor
+     * @param conversationState
+     */
+    public constructor(conversationState: ConversationState) {
+        super();
+        
+        this.conversationState = conversationState;
+        this.dialogState = conversationState.createProperty("dialogState");
+        this.dialogs = new DialogSet(this.dialogState);
+        this.dialogs.add(new HelpDialog("help"));
+
+        // Set up the Activity processing
+
+        this.onMessage(async (context: TurnContext): Promise<void> => {
+            // TODO: add your own bot logic in here
+            switch (context.activity.type) {
+                case ActivityTypes.Message:
+                    let text = TurnContext.removeRecipientMention(context.activity);
+                    text = text.toLowerCase();
+                    if (text.startsWith("mentionme")) {
+                        
+                        if (context.activity.conversation.conversationType == "personal") {
+                            await this.handleMessageMentionMeOneOnOne(context);
+                          } else if(context.activity.conversation.conversationType == "groupChat")
+                          {
+
+                            await this.handleMessageMentionMeGroupChatConversation(context);
+                          } 
+                          else  {
+                             
+                            await this.handleMessageMentionMeChannelConversation(context);
+                          }
+                          return;
+                    } else if (text.startsWith("hello")) {
+                        await context.sendActivity("Oh, hello to you as well!");
+                        return;
+                    } else if (text.startsWith("help")) {
+                        const dc = await this.dialogs.createContext(context);
+                        await dc.beginDialog("help");
+                    } else {
+                        await context.sendActivity(`I\'m terribly sorry, but my master hasn\'t trained me to do anything yet...`);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            // Save state changes
+            return this.conversationState.saveChanges(context);
+        });
+
+        this.onConversationUpdate(async (context: TurnContext): Promise<void> => {
+            if (context.activity.membersAdded && context.activity.membersAdded.length !== 0) {
+                for (const idx in context.activity.membersAdded) {
+                    if (context.activity.membersAdded[idx].id === context.activity.recipient.id) {
+                        const welcomeCard = CardFactory.adaptiveCard(WelcomeCard);
+                        await context.sendActivity({ attachments: [welcomeCard] });
+                    }
+                }
+            }
+        });
+
+        this.onMessageReaction(async (context: TurnContext): Promise<void> => {
+            const added = context.activity.reactionsAdded;
+            if (added && added[0]) {
+                await context.sendActivity({
+                    textFormat: "xml",
+                    text: `That was an interesting reaction (<b>${added[0].type}</b>)`
+                });
+            }
+        });;
+   }
+
+   private async handleMessageMentionMeOneOnOne(context: TurnContext): Promise<void> {
+    const mention = {
+      mentioned: context.activity.from,
+      text: `<at>${new TextEncoder().encode(context.activity.from.name)}</at>`,
+      type: "mention"
+    };
+  
+    const replyActivity = MessageFactory.text(`Hi ${mention.text} from a one to one personal chat.`);
+    replyActivity.entities = [mention];
+    await context.sendActivity(replyActivity);
+  }
+
+  private async handleMessageMentionMeChannelConversation(context: TurnContext): Promise<void> {
+    const mention = {
+      mentioned: context.activity.from,
+      text: `<at>${new TextEncoder().encode(context.activity.from.name)}</at>`,
+      type: "mention"
+    };
+  
+    const replyActivity = MessageFactory.text(`Hi ${mention.text}!`);
+    replyActivity.entities = [mention];
+    const followupActivity = MessageFactory.text(`*We are in a channel conversation*`);
+    await context.sendActivities([replyActivity, followupActivity]);
+  }
+
+  private async handleMessageMentionMeGroupChatConversation(context: TurnContext): Promise<void> {
+    const mention = {
+      mentioned: context.activity.from,
+      text: `<at>${new TextEncoder().encode(context.activity.from.name)}</at>`,
+      type: "mention"
+    };
+  
+    const replyActivity = MessageFactory.text(`Hi ${mention.text}!`);
+    replyActivity.entities = [mention];
+    const followupActivity = MessageFactory.text(`*We are in a Group Chat conversation*`);
+    await context.sendActivities([replyActivity, followupActivity]);
+  }
+  
+
+}
